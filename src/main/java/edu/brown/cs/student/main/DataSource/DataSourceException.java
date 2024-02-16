ce45f763f4da112a636842c07d86589aa8a61799
tanaysubramanian <tanay.subramanian@gmail.com>
2024-02-16 18:22:05 -0500

    Updated CacheManager

--- src/main/java/edu/brown/cs/student/main/DataSource/DataSourceException.java
+++ src/main/java/edu/brown/cs/student/main/DataSource/DataSourceException.java
@@ -1,3 +1,32 @@
 package edu.brown.cs.student.main.DataSource;
 
-public class DataSourceException extends Exception {}
+/**
+ * This class communicates errors with a requested datasource by wrapping the cause as a field.
+ */
+public class DataSourceException extends Exception {
+
+  private final Throwable cause;
+
+  public DataSourceException(String message) {
+    super(message); // Exception message
+    this.cause = null;
+  }
+
+  public DataSourceException(String message, Throwable cause) {
+    super(message); // Exception message
+    this.cause = cause;
+  }
+
+  public DataSourceException(Throwable cause) {
+    this.cause = cause;
+  }
+
+  /**
+   * Returns the Throwable provided as the root cause of this exception.
+   *
+   * @return the root cause Throwable
+   */
+  public Throwable getCause() {
+    return this.cause;
+  }
+}
